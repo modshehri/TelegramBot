0a7fa921ff6c560e200a023906c7eafa1da5fa6e
modshehri <mod.shehri@hotmail.com>
2021-03-28 02:24:59 +0300

    first commit

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf("1524672304:AAGrFCVPWhYycjq_KA9YYfuvbltVZ7vj4qg")
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+   functions.logger.info("Hello logs!", {structuredData: true});
+   response.send("Hello from Firebase!");
+ });
